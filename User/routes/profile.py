19d1d66a1905844525cb4b7fd9a250c63197d308
vladstor <vladstor231205@gmail.com>
2025-01-19 22:24:43 +0200

    user.d- ads+ jwt+

--- User/routes/profile.py
+++ User/routes/profile.py
@@ -4,37 +4,39 @@ from .. import user_bp
 from ..models import User
 
 @user_bp.route('/profile/<int:user_id>', methods=['GET'])
-@jwt_required()  # Додаємо захист за допомогою JWT
+@jwt_required()  
 def get_profile(user_id):
-    # Отримання ID поточного користувача з JWT
+
     current_user_id = get_jwt_identity()
 
-    # Отримання claims з JWT токена
+
     jwt_claims = get_jwt()
 
-    # Отримання ролі користувача з токена
+
     user_role = jwt_claims.get("role") 
 
     if not user_role:
         return jsonify({"error": "Role is missing in the token"}), 403
 
-    # Перевірка доступу: роль має бути "admin" або користувач має доступ до власного профілю
+
     if user_role == "admin":
-        # Адмін може отримати доступ до всіх профілів
+
         pass
+
     elif user_role == "user" and int(user_id) == int(current_user_id):
-        # Користувач може отримати доступ лише до свого профілю
+
         pass
+
     else:
         return jsonify({"error": "Access denied"}), 403
 
-    # Спроба знайти користувача в базі даних
+
     user = User.query.get(user_id)
 
     if not user:
         return jsonify({"error": "User not found"}), 404
 
-    # Отримуємо оголошення користувача
+
     ads_data = [
         {
             "title": ad.title,
@@ -47,9 +49,9 @@ def get_profile(user_id):
 
     # Повернення даних про користувача (спочатку користувач, потім оголошення)
     user_data = {
-        "username": user.username,
-        "email": user.email,
-        "phone": user.phone,
+        #"username": user.username,
+        #"email": user.email,
+        #"phone": user.phone,
         "ads": ads_data  # Інформація про оголошення після користувача
     }
 
