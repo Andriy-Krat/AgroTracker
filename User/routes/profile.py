9197da3f7e23e55e9b55ad1fd3cfb087dc86dcd3
vladstor <vladstor231205@gmail.com>
2025-01-19 19:05:46 +0200

    JWT+

--- User/routes/profile.py
+++ User/routes/profile.py
@@ -1,9 +1,33 @@
-from flask import jsonify
+from flask import jsonify, request
+from flask_jwt_extended import jwt_required, get_jwt, get_jwt_identity
 from .. import user_bp
 from ..models import User
 
 @user_bp.route('/profile/<int:user_id>', methods=['GET'])
+@jwt_required()  # Додаємо захист за допомогою JWT
 def get_profile(user_id):
+    # Отримання ID поточного користувача з JWT
+    current_user_id = get_jwt_identity()
+
+    # Отримання claims з JWT токена
+    jwt_claims = get_jwt()
+
+    # Отримання ролі користувача з токена
+    user_role = jwt_claims.get("role") 
+
+    if not user_role:
+        return jsonify({"error": "Role is missing in the token"}), 403
+
+    # Перевірка доступу: роль має бути "admin" або користувач має доступ до власного профілю
+    if user_role == "admin":
+        # Адмін може отримати доступ до всіх профілів
+        pass
+    elif user_role == "user" and int(user_id) == int(current_user_id):
+        # Користувач може отримати доступ лише до свого профілю
+        pass
+    else:
+        return jsonify({"error": "Access denied"}), 403
+
     # Спроба знайти користувача в базі даних
     user = User.query.get(user_id)
 
@@ -11,14 +35,15 @@ def get_profile(user_id):
         return jsonify({"error": "User not found"}), 404
 
     # Отримуємо оголошення користувача
-    ads_data = []
-    for ad in user.ads:
-        ads_data.append({
+    ads_data = [
+        {
             "title": ad.title,
             "location": ad.location,
             "price": ad.price,
             "description": ad.description
-        })
+        }
+        for ad in user.ads
+    ]
 
     # Повернення даних про користувача (спочатку користувач, потім оголошення)
     user_data = {
--- app.py
+++ app.py
@@ -1,13 +1,17 @@
 from flask import Flask, send_from_directory
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_jwt_extended import JWTManager
 from config import Config
+
 
 app = Flask(__name__)
 app.config.from_object(Config)
 
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+
+jwt = JWTManager(app)
 
 from ads.models import *
 from ads import ads_bp
